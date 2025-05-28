1ae2644d401220c79dba3f0266e10e683a1a0282
Mohanteja Dharmavarapu <mohantejadharmavarapu@Mohantejas-MacBook-Pro.local>
2025-05-28 19:14:53 +1000

    small underline fix on hire me in hero component

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,17 +3,6 @@ import './globals.css';
 import LayoutWrapper from '@/components/utils/LayoutWrapper';
 import { Toaster } from 'react-hot-toast';
 
-// const geistSans = Geist({
-//   variable: '--font-geist-sans',
-//   subsets: ['latin'],
-// });
-
-// const geistMono = Geist_Mono({
-//   variable: '--font-geist-mono',
-//   subsets: ['latin'],
-// });
-
-// ${geistSans.variable} ${geistMono.variable} 
 
 export const metadata: Metadata = {
   title: 'Mohanteja | Portfolio',
